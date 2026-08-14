a19ff820d8e168fd5eb86086403af5cc68d03d6e
Daniël van Vuuren <daniel.vanvuuren@e2mic.com>
2026-08-14 12:53:36 -0300

    Simplify mobile feed composer

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'boekoe-v30'
+const CACHE = 'boekoe-v31'
 const BASE = '/'
 const SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icon.svg`, `${BASE}icon-192.png`, `${BASE}icon-512.png`, `${BASE}icon-maskable-192.png`, `${BASE}icon-maskable-512.png`, `${BASE}apple-touch-icon.png`]
 
